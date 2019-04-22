f049dee68ae2cf65e2931c90e83c1b08b78e2b28
Hugo Woesthuis <hugow2011@outlook.com>
2019-04-22 13:42:19 +0200

    Update boot.asm

--- boot.asm
+++ boot.asm
@@ -2,7 +2,7 @@
 MBALIGN  equ  1 << 0            ; align loaded modules on page boundaries
 MEMINFO  equ  1 << 1            ; provide memory map
 FLAGS    equ  MBALIGN | MEMINFO ; this is the Multiboot 'flag' field
-MAGIC    equ  0x1BADB007        ; 'magic number' lets bootloader find the header
+MAGIC    equ  0x1BADB002        ; 'magic number' lets bootloader find the header
 CHECKSUM equ -(MAGIC + FLAGS)   ; checksum of above, to prove we are multiboot
 section .multiboot
 align 4
